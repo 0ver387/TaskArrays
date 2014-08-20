3ddbd6127de7bed0851f5427e5d85f3c044843d2
Oleg Leizer <oleizer@Olegs-MacBook-Pro.local>
2014-08-20 14:58:03 +0600

    Initial Commit

---
+++ Lesson 5 HomeTask/OLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OLAppDelegate.h
+//  Lesson 5 HomeTask
+//
+//  Created by Oleg Leizer on 20.08.14.
+//  Copyright (c) 2014 Oleg Leizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
